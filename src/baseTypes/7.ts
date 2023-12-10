d7512daf2b154a717f2d67ec7daf503ba7ab1bd8
OleksandrBalashov <aleksandr.balashov.sax@gmai.com>
2023-12-10 09:17:41 +0100

    finish

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,15 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum Day {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+const isWeekend = (day: Day) => day > 4;
--- src/generic/1.ts
+++ src/generic/1.ts
@@ -2,15 +2,15 @@
   Є функція getPromise(), яка повертає проміс, що дозволяється в масив, що містить рядки та числа. 
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
+type ResponcePromise<T extends (string | number)[]> = Promise<T>;
 
-function getPromise () {
+function getPromise(): ResponcePromise<[string, number]> {
   return new Promise((resolve) => {
-    resolve(['Text', 50]);
+    resolve(["Text", 50]);
   });
 }
 
-getPromise()
-.then((data) => {
+getPromise().then((data) => {
   console.log(data);
 });
 
